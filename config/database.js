a8b5c3830720b505c79ef77aa9e0a7c2b729577c
Laz <lazgerasimov@gmail.com>
2022-12-18 19:11:31 +0000

    .

---
+++ config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//TODO change db according to assignment
+const CONNECTION_STRING = 'mongodb://localhost:27017/scaffoldDb'
+
+module.exports = async (app) => {
+    try {
+        await mongoose.connect(CONNECTION_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Database connected');
+        
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+
+}
