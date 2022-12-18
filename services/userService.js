c606d4daa7384e5cb8b8723696cf0b0168ec0419
Laz <lazgerasimov@gmail.com>
2022-12-18 20:35:36 +0000

    add register func

---
+++ services/userService.js
@@ -0,0 +1,45 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'The most secret secret';
+
+//TODO replace with assignment
+async function register(username, password) {
+    const existing = await User.findOne({ username }).collation({ locale: 'en', strength: 2 });
+    if (existing) {
+        throw new Error('Username is taken');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10); // salt as second param
+    const user = await User.create({
+        username,
+        hashedPassword
+    });
+    //TODO check if registration creates user session
+    const token = createSession(user);
+    return token;
+}
+
+async function login() {
+
+}
+//TODO check assingment 
+function createSession(user) {   // create payload for cookie or has to manually log in
+    const payload = {
+        _id,
+        username: user.username,
+    };
+    const token = jwt.sign(payload, JWT_SECRET);
+    return token;
+}
+
+function verifyToken() {
+
+}
+
+
+module.exports = {
+    register,
+    login,
+    verifyToken
+}
